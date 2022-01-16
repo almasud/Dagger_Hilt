cb4e25c7ee17f639d738aef27e2a30cb30ff8224
almasud <almasud.dev@gmail.com>
2022-01-16 18:28:40 +0600

    Provide multiple injection of same type using qualifier

--- app/src/main/java/com/example/daggerhilt/services/Vehicle.kt
+++ app/src/main/java/com/example/daggerhilt/services/Vehicle.kt
@@ -5,31 +5,50 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 interface Media {
     fun getName() : String
 }
 
-// Constructor-injected, because Hilt needs to know how to
-// provide instances of Vehicle, too.
-class Vehicle @Inject constructor(private val name: String) : Media {
+// Constructor-injected
+class Vehicle @Inject constructor(
+    @FirstName
+    private val fistName: String,
+    @LastName
+    private val lastName: String
+    ) : Media {
 
     override fun getName(): String {
-        return name
+        return "$fistName $lastName"
     }
 }
 
 @Module
 @InstallIn(SingletonComponent::class)
 object MediaModule {
+    // Hilt doesn't care about method name. It's care return type
+    // And same return type more than once confuses to hilt what
+    // should be choose? For that reason we need to use qualifier
+    // as an annotation
+
 
     @Provides
     @Singleton
-    fun getName() : String = "Vehicle"
+    @FirstName
+    fun getFirstName() : String = "CNG"
 
     @Provides
-    fun provideMedia(
-        name: String
-    ) : Media = Vehicle(name)
+    @Singleton
+    @LastName
+    fun getLastName() : String = "Vehicle"
 }
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class FirstName
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class LastName
